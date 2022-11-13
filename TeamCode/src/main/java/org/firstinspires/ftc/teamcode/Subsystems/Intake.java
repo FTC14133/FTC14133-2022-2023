320b94d20206a1e2587611fa02eff4a66168f9a2
Vihaan Chhabria <72771042+VihaanChhabria@users.noreply.github.com>
2022-11-13 12:36:47 -0500

    Vihaan has deleted the extra servos for the intake. Noice!

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/Intake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/Intake.java
@@ -11,19 +11,15 @@ import org.firstinspires.ftc.robotcore.external.Telemetry;
 
 public class  Intake {
     // Instantiate the motor variables
-    CRServo intake_fl;
-    CRServo intake_fr;
-    CRServo intake_bl;
-    CRServo intake_br;
+    CRServo intake_f;
+    CRServo intake_b;
     boolean toggle = true;
     boolean Possession = true; //Variable telling whether we have possession of a game piece or not
     DigitalChannel beambreak; //The "beambreak" sensor is a type of IR sensor that detects if it vision is broken
 
     public Intake(HardwareMap hardwareMap){                 // Motor Mapping
-        intake_fl = hardwareMap.crservo.get("intake_fl");      //Sets the names of the hardware on the hardware map
-        intake_fr = hardwareMap.crservo.get("intake_fr");
-        intake_bl = hardwareMap.crservo.get("intake_bl");      //Sets the names of the hardware on the hardware map
-        intake_br = hardwareMap.crservo.get("intake_br");      //Sets the names of the hardware on the hardware map
+        intake_f = hardwareMap.crservo.get("intake_f");      //Sets the names of the hardware on the hardware map
+        intake_b = hardwareMap.crservo.get("intake_b");
         // "DeviceName" must match the Config EXACTLY
         beambreak = hardwareMap.get(DigitalChannel.class, "beambreak");
         // Set motor direction based on which side of the robot the motors are on
@@ -33,10 +29,8 @@ public class  Intake {
         if (positionArm > 0) {
             rotationSpeed *= -1;
         }
-        intake_fl.setPower(rotationSpeed);
-        intake_fr.setPower(-rotationSpeed);
-        intake_bl.setPower(rotationSpeed);
-        intake_br.setPower(-rotationSpeed);
+        intake_f.setPower(rotationSpeed);
+        intake_b.setPower(rotationSpeed);
     }
 
     public void Update_intake(double speed, int positionArm, Gamepad gamepad2){ //Standard intake function
