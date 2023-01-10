d861ab4bda3c10bf8444001c3d136fd37ca8224a
Vihaan Chhabria <72771042+VihaanChhabria@users.noreply.github.com>
2023-01-10 18:52:13 -0500

    Worked on Detection

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Reference/DetectionReference/JunctionAlignmentExample.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Reference/DetectionReference/JunctionAlignmentExample.java
@@ -1,21 +1,47 @@
 package org.firstinspires.ftc.teamcode.Reference.DetectionReference;
 
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
-import org.firstinspires.ftc.teamcode.Subsystems.Detections.Detection;
 
-@TeleOp(name="JunctionAlignmentExample", group="Iterative Opmode")
-public class JunctionAlignmentExample extends OpMode {
-    private org.firstinspires.ftc.teamcode.Subsystems.Detections.Detection Detection=null;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.teamcode.Subsystems.Detections.AprilTagDetectionPipeline;
+import org.firstinspires.ftc.teamcode.Subsystems.Detections.ColorDetectionPipeline;
+import org.openftc.easyopencv.OpenCvCamera;
+import org.openftc.easyopencv.OpenCvCameraFactory;
+import org.openftc.easyopencv.OpenCvCameraRotation;
 
-    public void init() {
-        Detection = new Detection(hardwareMap);
-        telemetry.addData("Object Creation", "Done");
-        Detection.setColorPipeline(telemetry);
-    }
+@TeleOp(name="JunctionAlignmentExample", group="Auto")
+public class JunctionAlignmentExample extends LinearOpMode {
+    OpenCvCamera camera;
+    ColorDetectionPipeline colorDetectionPipeline;
 
-    public void loop() {
-        Detection.junctionPos(telemetry);
-    }
+
+    public void runOpMode(){
+            int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+            camera = OpenCvCameraFactory.getInstance().createWebcam(hardwareMap.get(WebcamName.class, "Webcam 1"), cameraMonitorViewId);
+
+            camera.setPipeline(colorDetectionPipeline);
+            camera.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener()
+            {
+                @Override
+                public void onOpened()
+                {
+                    camera.startStreaming(800,448, OpenCvCameraRotation.UPRIGHT);
+                }
+
+                @Override
+                public void onError(int errorCode)
+                {
+
+                }
+            });
+
+            telemetry.setMsTransmissionInterval(50);
+
+            while (!isStarted() && !isStopRequested()){
+                colorDetectionPipeline.returnJunctionPos(telemetry);
+            }
+        }
 }
